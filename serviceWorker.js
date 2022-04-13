07ad475d69941244c72b687c26178e2ff4ad5c66
T T <81670340+simplytobo@users.noreply.github.com>
2022-04-13 09:21:37 +0000

    pWA logo

--- serviceWorker.js
+++ serviceWorker.js
@@ -54,7 +54,7 @@ self.addEventListener('fetch', (event) => {
     })
   );
 }); */
-/* self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.open('mysite-dynamic').then(function(cache) {
       return fetch(event.request).then(function(response) {
@@ -63,7 +63,7 @@ self.addEventListener('fetch', (event) => {
       });
     })
   );
-}); */
+});
 
 
  self.addEventListener("install", function(event) {
